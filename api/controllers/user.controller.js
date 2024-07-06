e2007933c6c0317da8b42fa2bca20c7715be11e1
Pranjul Pal <130579601+itsPranjul@users.noreply.github.com>
2024-07-06 17:48:41 +0530

    Done app

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
 
 export const test = (req, res) => {
+  
   res.json({ message: 'API is working!' });
 };
 
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "blogg-app-22c35.firebaseapp.com",
   projectId: "blogg-app-22c35",
   storageBucket: "blogg-app-22c35.appspot.com",
